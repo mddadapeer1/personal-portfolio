0b231ce0d350c536910eb3f2472c59d29666bad3
Md Dadapeer C <127332734+mddadapeer1@users.noreply.github.com>
2024-11-07 12:28:31 +0530

    Update profile.js

--- src/mock/profile.js
+++ src/mock/profile.js
@@ -1,23 +1,23 @@
 export const SOCIAL_LINKS = [
   {
-    link: 'https://github.com/SAI-MOHAN-B',
+    link: https://github.com/mddadapeer1',
     icon: 'akar-icons:github-fill',
   },
   {
-    link: 'https://www.linkedin.com/in/saimohanb/',
+    link: 'https://www.linkedin.com/in/md-dadapeer-c/',
     icon: 'akar-icons:linkedin-fill',
   },
   {
-    link:'https://drive.google.com/file/d/1lqA5Ly1vZCMMA0WOzOwegBXtEGQKKPfD/view?usp=sharing',
+    link:'https://drive.google.com/file/d/1cfxOOdDfMwcao1UFYAxhIulPQP8otXRK/view?usp=sharing',
     icon:'flat-color-icons:download'
   }
 ];
 
-export const HERO_TITLES = ['SAI MOHAN B', 'Data Engineer', 'AI Enthusiast'];
+export const HERO_TITLES = ['Md Dadapeer C', 'DevOps Engineer', 'Cloud Enthusiast'];
 
 export const aboutParagraph =
-  'Highly motivated and enthusiastic Data Engineer with 3+ years of experience in designing, developing, and maintaining data pipelines and architectures using technologies such as Python, SQL, AWS, Apache Spark. Skilled in building scalable data solutions, working with large datasets, and implementing ETL processes to ensure efficient data flow and availability for analytics';
+  'Highly motivated and enthusiastic DevOps Engineer with 3+ years of expertise in deploying and managing cloud-based Infrastructure using Amazon Web Services.   Skilled in developing robust CI/CD pipelines, optimizing Build processes, and leveraging DevOps tools for actionable business insights. Proven ability to streamline data workflows and collaborate with cross-functional teams';
 
-export const contactEmail = 'bmohan048@gmail.com';
+export const contactEmail = 'mdc.dadapeer@gmail.com';
 
-export const documentTitle = 'Hello I am  SAI MOHAN B | Data Engineer | AI Enthusiast'
+export const documentTitle = 'Hello I am  Md Dadapeer | DevOps Engineer | Cloud Enthusiast'
